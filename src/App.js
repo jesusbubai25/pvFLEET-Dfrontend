fd9dfcd2045d6b0191b8ca2dc4918d74659db3c1
sidhantnahak2 <snahak@greenenco.co.uk>
2024-04-30 16:02:05 +0530

    footer content added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <>
       <div className="app">
         <Leafleet />
+        <div className="footer">
+         <span>&#169;</span>{" "} <a target="_blank" href="https://greenenco.co.uk">greenenco.co.uk.</a>&nbsp;All Rights Reserved.
+        </div>
       </div>
     </>
   );
